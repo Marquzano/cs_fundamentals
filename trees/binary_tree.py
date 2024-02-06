f3d89f9b3da37302e0c0c1d31f9e5087db3e44fa
Marquzano <angel.angelm.marquez3@gmail.com>
2024-02-06 16:11:49 -0600

    added Node and BinaryTree classes

--- trees/binary_tree.py
+++ trees/binary_tree.py
@@ -1,2 +1,12 @@
 # here we will define a basic binary tree
 # as well as it's common operations
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def __init__(self,root):
+        self.root = Node(root)
